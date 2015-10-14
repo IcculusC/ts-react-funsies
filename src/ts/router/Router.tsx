0c6beaca78cde2f09acf2d3b3909663bc78b8a17
Leath Cooper <leathcooper@gmail.com>
2015-10-13 23:23:51 -0700

    fleshed out the router class

--- src/ts/router/Router.tsx
+++ src/ts/router/Router.tsx
@@ -3,11 +3,13 @@
 
 import React = require('react');
 import ReactRouter = require('react-router');
+import AppDispatcher = require('../dispatchers/AppDispatcher');
 
 class Router {
 
   private static _instance: Router;
   private static _router: any;
+  private _appToken: string;
   
   constructor(routes: any) {
     if(Router._instance) {
@@ -17,9 +19,11 @@ class Router {
     Router._instance = this;
     
     Router._router = ReactRouter.create(routes);
+    
+    this._appToken = AppDispatcher.register(this._handleDispatch.bind(this));
   }
   
-  static get instance(): Router {
+  static get instance(): Router {  
     return Router._instance;
   }
   
@@ -28,28 +32,40 @@ class Router {
   }
   
   makePath(to, params, query) {
+    AppDispatcher.dispatch({ action: 'router', payload: { type: 'makePath', target: to, params: params, query: query }});
+  
     return Router._router.makePath(to, params, query);
   }
   
   makeHref(to, params, query) {
+    AppDispatcher.dispatch({ action: 'router', payload: { type: 'makeHref', target: to, params: params, query: query }});
+  
     return Router._router.makeHref(to, params, query);
   }
   
-  transitionTo(to, params, query) {
-      Router._router.transitionTo(to, params, query);
+  transitionTo(to: string, params, query) {
+    AppDispatcher.dispatch({ action: 'router', payload: { type: 'transitionTo', target: to, params: params, query: query }});  
+  
+    Router._router.transitionTo(to, params, query);
   }
 
   replaceWith(to, params, query) {
-      Router._router.replaceWith(to, params, query);
+    AppDispatcher.dispatch({ action: 'router', payload: { type: 'replaceWith', target: to, params: params, query: query }});
+  
+    Router._router.replaceWith(to, params, query);
   }
 
   goBack() {
-      Router._router.goBack();
+    AppDispatcher.dispatch({ action: 'router', payload: { type: 'goBack' }});
+    
+    Router._router.goBack();
   }
 
   run(render) {
       Router._router.run(render);
   }
+  
+  _handleDispatch(payload): void {}
 }
 
 export = Router;
