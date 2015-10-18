c05d30ecb2ecbf88f5925f439b8dfc19d4bf07bd
Leath Cooper <leathcooper@gmail.com>
2015-10-18 02:05:42 -0700

    fleshing out flux

--- src/ts/flux/Dispatcher.tsx
+++ src/ts/flux/Dispatcher.tsx
@@ -1,7 +1,37 @@
-/// <reference path='../typings/eventemitter3/eventemitter3.d.ts' />
+/// <reference path='../typings/Q/Q.d.ts' />
 
-import EventEmitter = require('eventemitter3');
+import Q = require('q');
 
-class Dispatcher extends EventEmitter {
+class Dispatcher<T> {
+  private _callbacks = [];
+  private _promises = [];
 
+  register(callback: () => void): number {
+    this._callbacks.push(callback);
+    return this._callbacks.length - 1;
+  }
+  
+  dispatch(payload: T) {
+    let resolves = [];
+    let rejects = [];
+    
+    this._promises = this._callbacks.map(function(_, i) {
+      return Q.Promise(function(resolve, reject) {
+        resolves[i] = resolve;
+        rejects[i] = rejects;
+      });
+    });
+    
+    this._callbacks.forEach(function(callback, i) {
+      Q.resolve(callback(payload)).then(function() {
+        resolves[i](payload);
+      }, function() {
+        rejects[i](new Error('Dispatcher callback unsuccessful'));
+      });
+    });
+    
+    this._promises = [];
+  }
 }
+
+export = Dispatcher;
--- src/ts/flux/Store.tsx
+++ src/ts/flux/Store.tsx
@@ -1,7 +1,23 @@
 /// <reference path='../typings/eventemitter3/eventemitter3.d.ts' />
 
 import EventEmitter = require('eventemitter3');
+import Dispatcher = require('./Dispatcher');
 
 class Store extends EventEmitter {
 
+  private _dispatcher: Dispatcher<any>;
+  
+  constructor(dispatcher: Dispatcher<any>) {
+    this._dispatcher = dispatcher;
+    
+    super();
+  }
+  
+  get dispatcher() {
+    return this._dispatcher;
+  }
+  
+  _handleDispatch(payload: {}) {
+    
+  }
 }
